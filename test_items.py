8c7e0da68e5b13f7e51af2d760b70639092a4f0e
Milendar <montallaar@gmail.com>
2023-07-31 06:37:34 +0300

    FindItem1

--- test_items.py
+++ test_items.py
@@ -8,6 +8,7 @@ from selenium.webdriver.support import expected_conditions as EC
 def test_add_to_cart_button(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
+    time.sleep(30)
 
     # Проверяем наличие кнопки добавления в корзину
     try:
